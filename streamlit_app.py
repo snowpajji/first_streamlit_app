574ba3ac4ef9bef9ba3815dc1a79c49d8024969e
snowpajji <126487197+snowpajji@users.noreply.github.com>
2023-02-27 19:54:05 +0530

    Update streamlit_app.py
    fixed_typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title("my parents new healthy dinner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & blueberry oatmeal")
+streamlit.text("Kale, Spinach & Rocket smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
